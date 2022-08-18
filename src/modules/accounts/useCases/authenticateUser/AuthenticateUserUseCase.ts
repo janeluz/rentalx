08a302507a2a2be00f5056dcf7c5c1720b1a0c06
Jane Luz <janerodrigues_2610@hotmail.com>
2022-08-17 23:47:52 -0300

    add alterações

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -38,10 +38,15 @@ class AuthenticateUserUseCase {
       subject: user.id,
       expiresIn: "1d",
     });
-    return {
-      user,
+    const tokenReturn: IResponse = {
       token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
     };
+    return tokenReturn;
   }
 }
+
 export { AuthenticateUserUseCase };
