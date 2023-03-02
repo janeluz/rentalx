e671a008aec194f1d095216bccea6051cb84748b
Jane Luz <janerodrigues_2610@hotmail.com>
2023-03-02 10:14:24 -0300

    chore: add refreshToken Usecase

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -11,7 +11,7 @@ class UsersTokensRepository implements IUsersTokensRepository {
   constructor() {
     this.repository = AppDataSource.getRepository(UserTokens);
   }
-
+     
   async create({
     expires_date,
     refresh_token,
@@ -24,6 +24,16 @@ class UsersTokensRepository implements IUsersTokensRepository {
     });
     await this.repository.save(userToken);
     return userToken;
+  }
+  async findByUserIdAndRefreshToken(user_id: string,refresh_token:string): Promise<UserTokens> {
+   const userTokens = await this.repository.findOne({
+    user_id,
+    refresh_token,
+    });
+    return userTokens;
+  }
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
   }
 }
 export { UsersTokensRepository };
--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -2,11 +2,15 @@ import { ICreateUserTokenDto } from '../dtos/ICreateUserTokenDTO';
 import { UserTokens } from '../infra/typeorm/entities/UsersToken';
 
 interface IUsersTokensRepository {
+  deleteById(id: string): unknown;
+  
   create({
     expires_date,
     refresh_token,
     user_id,
   }: ICreateUserTokenDto): Promise<UserTokens>;
+  findByUserIdAndRefreshToken(user_id: string, refresh_token:string): Promise<UserTokens>;
+  deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
